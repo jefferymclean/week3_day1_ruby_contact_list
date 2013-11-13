314a3e87692d12676e5083393bfe7e1d794c1e0c
Jeffery McLean <jeffery.mclean@gmail.com>
2013-11-13 00:29:31 +0000

    quit command

--- application.rb
+++ application.rb
@@ -3,21 +3,31 @@ class Application
   def initialize
     # Start with an empty array of contacts.
     # TODO: Perhaps stub (seed) some contacts so we don't have to create some every time we restart the app
-    @contacts = []
+    @contacts = ["Rich" "Jeff" "Sean" "Jessica" "Colin"]
   end
  
   def run
-    show_main_menu
-    input = gets.chomp
+    loop do
+      show_main_menu
+      input = gets.chomp
+      break if input == "quit"
+    end
   end
   
   # Prints the main menu only
+   
   def show_main_menu
-    puts "Welcome to the app. What's next?"
+    puts "Welcome to your app. What's next?"
     puts " new      - Create a new contact"
     puts " list     - List all contacts"
     puts " show :id - Display contact details"
     print "> "
   end
- 
+
 end
+
+
+# if input = "quit"
+#      abort("Message goes here")
+#     else 
+#      return 
--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
 require_relative 'contact'
 require_relative 'application'
  
-Application.new.run
+Application.new.run 
