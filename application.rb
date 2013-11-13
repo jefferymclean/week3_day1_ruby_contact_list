3316c07ba84eb4ca04888ffc84cb29641a1d8566
Jeffery McLean <jeffery.mclean@gmail.com>
2013-11-13 21:54:21 +0000

    finally done list task

--- application.rb
+++ application.rb
@@ -2,20 +2,36 @@ class Application
  
   def initialize
     # Start with an empty array of contacts.
-    # TODO: Perhaps stub (seed) some contacts so we don't have to create some every time we restart the app
-    @contacts = ["Rich" "Jeff" "Sean" "Jessica" "Colin"]
+      
+    @contacts = [Contact.new("Jeff McLean", "jeffery.mclean@gmail.com")]
+                  # ^ this is the instance of the class (saved in an array)
   end
  
   def run
     loop do
-      show_main_menu
+      show_main_menuqu
       input = gets.chomp
       break if input == "quit"
-    end
-  end
-  
+      if input == "new"
+        puts "Please enter your first and last names."
+        input_name = gets.chomp
+        if input_name.length > 0
+          puts "Please enter your email address."
+          input_email = gets.chomp
+          puts @contacts 
+          new_contact = Contact.new(input_name, input_email)
+          @contacts << new_contact
+        end
+
+      elsif input == "list"
+        @contacts.each_with_index do |input_name, input_email| 
+          puts "#{input_name}: #{input_email}" 
+        end
+      end
+    end 
+  end  
+
   # Prints the main menu only
-   
   def show_main_menu
     puts "Welcome to your app. What's next?"
     puts " new      - Create a new contact"
@@ -23,11 +39,4 @@ class Application
     puts " show :id - Display contact details"
     print "> "
   end
-
 end
-
-
-# if input = "quit"
-#      abort("Message goes here")
-#     else 
-#      return 
--- contact.rb
+++ contact.rb
@@ -1,15 +1,26 @@
 class Contact
   
-  attr_accessor :first_name
-  attr_accessor :last_name
+  attr_accessor :name
   attr_accessor :email
   
-  def initialize(name, email)
-    # TODO: assign local variables to instance variables
+  def initialize(input_name, input_email)
+  	# assign local variables to instance variables
+  	@input_name = input_name
+  	@input_email = input_email
   end
+
+  def input_name 
+  		"#{@input_name}"
+  end
+
+    def input_email 
+  		"#{@input_email}"
+  end
+
   
   def to_s
-    # TODO: return string representation of Contact
+    # return string representation of Contact
+    "#{input_name} (#{input_email})"
   end
   
 end
